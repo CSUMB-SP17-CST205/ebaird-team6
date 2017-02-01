1941859bb3cfea53b3f563bb91dac0317ff1d3c4
Elijah Baird <ebaird@csumb.edu>
2017-02-01 23:49:07 +0000

    starting code

--- Project1.py
+++ Project1.py
@@ -1,2 +1,12 @@
-print ("hello world")
 #(* v *)
+from PIL import Image
+
+im = Image.open("1.png")
+rgb_im = im.convert('RGB')
+r, g, b = rgb_im.getpixel((0, 0))
+width, height = im.size
+
+print r, g, b
+print width, height
+
+rgb = []
